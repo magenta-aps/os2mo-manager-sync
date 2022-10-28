caa18c8cdcf7beda68b046cd69e87e69e581f93b
Michael Søegaard <ms@magenta-aps.dk>
2022-10-28 11:48:34 +0200

    [#46814] Base FastAPI app created with graphql client

---
+++ tests/test_main.py
@@ -0,0 +1,50 @@
+# SPDX-FileCopyrightText: 2022 Magenta ApS <https://magenta.dk>
+# SPDX-License-Identifier: MPL-2.0
+from collections.abc import Callable
+from collections.abc import Generator
+from typing import Any
+
+import pytest
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+from sd_managerscript.main import create_app
+
+
+@pytest.fixture
+def fastapi_app_builder() -> Generator[Callable[..., FastAPI], None, None]:
+    """Fixture for the FastAPI app builder."""
+
+    def builder(*args: Any, default_args: bool = True, **kwargs: Any) -> FastAPI:
+        if default_args:
+            kwargs["client_secret"] = "hunter2"
+        return create_app(*args, **kwargs)
+
+    yield builder
+
+
+@pytest.fixture
+def test_client_builder(
+    fastapi_app_builder: Callable[..., FastAPI]
+) -> Generator[Callable[..., TestClient], None, None]:
+    """Fixture for the FastAPI test client builder."""
+
+    def builder(*args: Any, **kwargs: Any) -> TestClient:
+        return TestClient(fastapi_app_builder(*args, **kwargs))
+
+    yield builder
+
+
+@pytest.fixture
+def test_client(
+    test_client_builder: Callable[..., TestClient]
+) -> Generator[TestClient, None, None]:
+    """Fixture for the FastAPI test client."""
+    yield test_client_builder(client_secret="hunter2")
+
+
+async def test_index(test_client: TestClient) -> None:
+    """Test the root endpoint on our app."""
+    response = test_client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Integration": "SD Managersync"}
